dddedb090d1476bd7449361d3021fda154e10d7d
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-09-29 12:22:19 +0200

    add first commit

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,24 +1,20 @@
 import './App.css';
-import logo from './assets/holberton-logo.jpg';
+import holbertonLogo from './assets/holberton-logo.jpg';
 
 function App() {
-  const year = new Date().getFullYear();
-
   return (
-    <>
+    <div className="App">
       <div className="App-header">
-        <img src={logo} alt="holberton logo" />
+        <img src={holbertonLogo} alt="holberton logo" />
         <h1>School dashboard</h1>
       </div>
-
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-
       <div className="App-footer">
-        <p>Copyright {year} - holberton School</p>
+        <p>Copyright {new Date().getFullYear()} - holberton School</p>
       </div>
-    </>
+    </div>
   );
 }
 
