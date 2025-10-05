be56808ab0b1d4139c60b1a8652865c9a1f58dd9
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-10-05 21:08:16 +0200

    modif test

--- react_props/task_0/dashboard/src/Notifications/Notifications.jsx
+++ react_props/task_0/dashboard/src/Notifications/Notifications.jsx
@@ -1,34 +1,32 @@
 import React from 'react';
 import './Notifications.css';
-import closeIcon from '../assets/close-button.png';
-import { getLatestNotification } from '../utils/utils';
+import closeIcon from '../assets/close-icon.png';
 
 function Notifications() {
-  const handleCloseClick = () => {
-    console.log('Close button has been clicked');
-  };
+  const handleClick = () => console.log('Close button has been clicked');
 
   return (
-    <div className="notification-items">
+    <div className="Notifications">
       <button
         style={{
           position: 'absolute',
           top: '10px',
           right: '10px',
-          background: 'transparent',
+          background: 'none',
           border: 'none',
-          cursor: 'pointer'
+          cursor: 'pointer',
         }}
         aria-label="Close"
-        onClick={handleCloseClick}
+        onClick={handleClick}
       >
-        <img src={closeIcon} alt="close" />
+        <img src={closeIcon} alt="close icon" width="15px" />
       </button>
+
       <p>Here is the list of notifications</p>
       <ul>
-        <li data-priority="default">New course available</li>
-        <li data-priority="urgent">New resume available</li>
-        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+        <li>New course available</li>
+        <li>New resume available</li>
+        <li>New data available</li>
       </ul>
     </div>
   );
