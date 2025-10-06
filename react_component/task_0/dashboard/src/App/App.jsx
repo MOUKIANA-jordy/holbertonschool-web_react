dd7e27b2e6b8a75600598048f3b5597b4ce5bc08
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-10-06 12:21:13 +0200

    first commit

---
+++ react_component/task_0/dashboard/src/App/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Notifications />
+        <div className="App">
+          <Header />
+          <Login />
+          <Footer />
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
