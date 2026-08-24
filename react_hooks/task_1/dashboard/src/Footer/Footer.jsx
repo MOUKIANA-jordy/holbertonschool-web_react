364c3c8a0fe93060faddecb1ee6013b1d261671a
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-08-24 17:27:17 +0200

    Convert Login component to React hooks

---
+++ react_hooks/task_1/dashboard/src/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import { useContext } from 'react';
+import NewContext from '../Context/context';
+import {
+  getCurrentYear,
+  getFooterCopy,
+} from '../utils/utils';
+
+function Footer() {
+  const { user } = useContext(NewContext);
+
+  return (
+    <footer className="App-footer mt-auto w-full border-t-[3px] border-[var(--main-color)] bg-white p-4 text-center text-sm italic">
+      <p>
+        Copyright {getCurrentYear()} -{' '}
+        {getFooterCopy(true)}
+      </p>
+
+      {user && user.isLoggedIn && (
+        <p>
+          <a
+            href="mailto:contact@holbertonschool.com"
+            className="text-[var(--main-color)] underline"
+          >
+            Contact us
+          </a>
+        </p>
+      )}
+    </footer>
+  );
+}
+
+export default Footer;
